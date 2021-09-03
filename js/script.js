a82187ff8f512fd3c68666860527e78e15b7195b
frederikke-m <frederikke.b.mathiesen@gmail.com>
2021-09-03 08:50:12 +0200

    åben menu virker

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-const btnHamburger = document.querySelector("#btnHamburger");
+// const btnHamburger = document.querySelector("#btnHamburger");
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// btnHamburger.addEventListener("click", myFunction)
+
+const btnHamburger = document.querySelector("#btnHamburger");
+const overlay = document.querySelector(".overlay")
+const overlayContent = document.querySelector(".overlay-content")
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+
+  if (overlay.classList.contains("show")) {
+    overlay.classList.remove("show")
+    overlayContent.classList.remove("show")
+  } else {
+    overlay.classList.add("show")
+    overlayContent.classList.add("show")
+  }
 }
 
-btnHamburger.addEventListener("click", myFunction)
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+btnHamburger.addEventListener("click", openNav)
