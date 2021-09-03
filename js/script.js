cb9285a9df577145be655ab534633d7840f5f4ff
frederikke-m <frederikke.b.mathiesen@gmail.com>
2021-09-03 09:42:32 +0200

    menu mangler slide down effekt

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@ const overlayContent = document.querySelector(".overlay-content")
 const btnClose = document.querySelector("#btnClose")
 
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-
   if (overlay.classList.contains("show")) {
     overlay.classList.remove("show")
     overlayContent.classList.remove("show")
@@ -27,11 +25,6 @@ function openNav() {
   }
 }
 
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
-
 btnHamburger.addEventListener("click", openNav)
-btnHamburger.addEventListener("click", closeNav)
 
 
