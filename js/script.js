8336229a1a258ca23b1ae0903320af152d79dc1a
frederikke-m <frederikke.b.mathiesen@gmail.com>
2021-09-11 15:18:50 +0200

    active links class tilføjet

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
-// const btnHamburger = document.querySelector("#btnHamburger");
-// const overlay = document.querySelector(".overlay")
-// const overlayContent = document.querySelector(".overlay-content")
-// const menuLinks = document.querySelectorAll("a");
-
-// btnHamburger.addEventListener("click", function () {
-//   if (overlay.classList.contains("show")) {
-//     /* Luk hamburger menu
-//     ============================*/
-//     overlay.classList.remove("show");
-//     overlay.classList.add("hidden");
-//     overlayContent.classList.remove("show");
-//     overlayContent.classList.add("hidden");
-//   } else {
-//     /* Åben hamburger menu
-//     ============================*/
-//     overlay.classList.remove("hidden");
-//     overlay.classList.add("show");
-//     overlayContent.classList.remove("hidden");
-//     overlayContent.classList.add("show");
-//   }
-// });
-
 const body = document.querySelector("body");
 const btnHamburger = document.querySelector("#btnHamburger");
 const headerMenu = document.querySelector(".header__menu");
-const languageElem = document.querySelector(".header__language");
 const header = document.querySelector("header");
 const home = document.querySelector(".home");
 const whoIAm = document.querySelector(".whoiam");
@@ -55,12 +31,27 @@ function hamburgerMenu () {
   }
 }
 
+// Active class på links
+const navlinks = document.querySelectorAll(".header__links");
+console.log(navlinks)
+const sections = Array.from(document.querySelectorAll("section"));
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 200 < sections[index].offsetTop) {}
+
+  navlinks.forEach((link) => link.classList.remove("active"));
+  navlinks[index].classList.add("active");
+}
+
+changeLinkState();
+window.addEventListener("scroll", changeLinkState);
 
 
 
 
-
-//Smooth scroll
+//Smooth scroll – virker ikke? :(
 
 const arrowHero = document.querySelector(".arrowhero")
 
